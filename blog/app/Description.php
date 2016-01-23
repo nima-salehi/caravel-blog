7afaf40aa407dcb81a48763fc7266e5fbe22aaae
nima-salehi <nima-salehi@hotmail.com>
2016-01-23 15:46:33 +0330

    creating model

---
+++ blog/app/Description.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Description extends Model
+{
+    //creating model
+    return $query->where('product_id', $productId);
+    // 
+}
